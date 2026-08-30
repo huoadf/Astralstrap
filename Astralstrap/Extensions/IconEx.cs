cd148fe95ad6c126567409c683ba9abfccaa9217
Astralstrap <contact@astralstrap.local>
2026-08-30 17:41:33 +0100

    fix(icon): use native HIcon and Bitmap conversion to prevent BitmapDecoder FileFormatException

--- Astralstrap/Extensions/IconEx.cs
+++ Astralstrap/Extensions/IconEx.cs
@@ -1,36 +1,51 @@
-﻿using System.Drawing;
+using System.Drawing;
 using System.IO;
-using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Bloxstrap.Extensions
 {
     public static class IconEx
     {
-        public static Icon GetSized(this Icon icon, int width, int height) => new(icon, new Size(width, height));
+        public static Icon GetSized(this Icon icon, int width, int height) => new(icon, new System.Drawing.Size(width, height));
 
         public static ImageSource GetImageSource(this Icon icon, bool handleException = true)
         {
-            using MemoryStream stream = new();
-            icon.Save(stream);
-            stream.Seek(0, SeekOrigin.Begin);
-
-            if (handleException)
+            try
             {
+                var bitmapSource = Imaging.CreateBitmapSourceFromHIcon(
+                    icon.Handle,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                bitmapSource.Freeze();
+                return bitmapSource;
+            }
+            catch (Exception ex)
+            {
+                App.Logger.WriteException("IconEx::GetImageSource", ex);
+
                 try
                 {
-                    return BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                    using var bmp = icon.ToBitmap();
+                    using var ms = new MemoryStream();
+                    bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    var bmpImg = new BitmapImage();
+                    bmpImg.BeginInit();
+                    bmpImg.StreamSource = ms;
+                    bmpImg.CacheOption = BitmapCacheOption.OnLoad;
+                    bmpImg.EndInit();
+                    bmpImg.Freeze();
+                    return bmpImg;
                 }
-                catch (Exception ex)
+                catch
                 {
-                    App.Logger.WriteException("IconEx::GetImageSource", ex);
-                    Frontend.ShowMessageBox(string.Format(Strings.Dialog_IconLoadFailed, ex.Message));
-                    return BootstrapperIcon.IconBloxstrap.GetIcon().GetImageSource(false);
+                    return new BitmapImage(new Uri("pack://application:,,,/Astralstrap.png"));
                 }
-            }
-            else
-            {
-                return BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
             }
         }
     }
